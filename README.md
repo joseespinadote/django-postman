70e7280e93d733a450c01fc0b50f9a163059db67
José en el asus zenbook <el.charlie@gmail.com>
2021-03-08 17:03:16 -0300

    version funcional

--- README.md
+++ README.md
@@ -13,4 +13,14 @@ arg3 = subject
 
 ## example
 
-python manage.py runscript enviar --script-args template.html recipients.csv "Prueba 2"
+python manage.py runscript enviar --script-args template_massive.html recipients.csv "Prueba"
+
+. Enviar mail con estilo e imágen
+. Hacer uso de variables en el template
+
+A continuación, te indicamos los pasos para que actives tu cuenta institucional.
+
+1. Ingresa a www.gmail.com
+2. Haz login utilizando tu correo electrónico XXXXX, siendo tu password temporal el RUT (sin puntos, ni guion y sin dígito verificador)
+
+Cuando ingreses por primera vez, se te pedirá actualizar la contraseña
--- scripts/enviar.py
+++ scripts/enviar.py
@@ -3,7 +3,9 @@ from django.template import Context
 from django.template.loader import render_to_string, get_template
 from django.core.mail import EmailMessage
 from django.conf import settings
+from smtplib import SMTPException
 from email.mime.image import MIMEImage
+import logging
 import os
 import csv
 
@@ -28,32 +30,48 @@ def run(*args):
     mail_title = args[2]
 
     recipients = []
-    with open(path_recipients, encoding='utf-8', newline='') as f:
+    with open(path_recipients, encoding='iso-8859-1', newline='') as f:
         reader = csv.reader(f)
         recipients = list(reader)
 
     image_filename='logo_uaysen_01.jpg'
+    es_header = True
+    for recipient in recipients :
+        if es_header :
+            es_header = False
+            continue
+        alumno = recipient[4]
+        alumno_mail_personal = recipient[6]
+        json_context = {
+            'nombre_alumno' : alumno.title(),
+            'email_institucional' : recipient[5],
+            'image_filename': image_filename
+        }
+        path_html_template = os.path.join(settings.BASE_DIR, path_template)
+        message = get_template(path_html_template).render(json_context)
 
-    json_context = {
-        'user': 'alumn@',
-        'image_filename': image_filename
-    }
-    path_html_template = os.path.join(settings.BASE_DIR, path_template)
-    message = get_template(path_html_template).render(json_context)
+        print(alumno, end=",")
+        print(alumno_mail_personal, end=",")
+        print(recipient[5])
+        
+        email = EmailMessage(
+            mail_title,
+            message,
+            'no-responder@uaysen.cl',
+            [alumno_mail_personal],
+            ['jose.espina@uaysen.cl'],
+            reply_to=['no-responder@uaysen.cl'],
+        )
+        with open(os.path.join(settings.BASE_DIR, 'static/'+image_filename), mode='rb') as f:
+            image = MIMEImage(f.read())
+            email.attach(image)
+            image.add_header('Content-ID', f"<{image_filename}>")
+        email.content_subtype = "html"
 
-    email = EmailMessage(
-        mail_title,
-        message,
-        'no-responder@uaysen.cl',
-        ['gitlab@uaysen.cl'],
-        [recipient[0] for recipient in recipients],
-        reply_to=['no-responder@uaysen.cl'],
-    )
-    with open(os.path.join(settings.BASE_DIR, 'static/'+image_filename), mode='rb') as f:
-        image = MIMEImage(f.read())
-        email.attach(image)
-        image.add_header('Content-ID', f"<{image_filename}>")
-    
-    email.content_subtype = "html"
-    email.send()
+        try:
+            email.send()
+        except SMTPException as e:
+            logging.error('This is an error message', e)
+        
+    return
     
